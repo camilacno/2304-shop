78568042fdea83f14be16ab533bda4b38a31b324
Camila Nepomuceno <camila.cno@gmail.com>
2022-12-01 12:14:30 -0300

    App header

---
+++ src/styles/pages/app.ts
@@ -0,0 +1,17 @@
+import { styled } from '..'
+
+export const Container = styled('div', {
+  minHeight: '100vh',
+
+  display: 'flex',
+  alignItems: 'flex-start',
+  flexDirection: 'column',
+  justifyContent: 'center',
+})
+
+export const Header = styled('header', {
+  margin: '0 auto',
+  maxWidth: 1180,
+  padding: '2rem 0',
+  width: '100%',
+})
