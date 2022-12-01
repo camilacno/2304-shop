1e415338c09c4f0f48d6c55b5615818171fe37d2
Camila Nepomuceno <camila.cno@gmail.com>
2022-12-01 20:12:10 -0300

    SSG dinamic parameter (product) + fallback

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,6 @@
-import { GetStaticProps } from 'next'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import Stripe from 'stripe'
 import { stripe } from '../../libs/stripe'
 import {
@@ -19,6 +20,12 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter()
+
+  if (isFallback) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
@@ -33,6 +40,19 @@ export default function Product({ product }: ProductProps) {
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      // {
+      //   params: {
+      //     id: 'most selled, preferred, etc., product ids',
+      //   },
+      // },
+    ],
+    fallback: true,
+  }
 }
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
