2a68a0f3d5d6916cf491760a84f51f7727359b27
Camila Nepomuceno <camila.cno@gmail.com>
2022-12-01 13:58:53 -0300

    Home structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+import { HomeContainer, Product } from '../styles/pages/home'
+import shirt1 from '../assets/shirts/1.png'
+import shirt2 from '../assets/shirts/2.png'
+import shirt3 from '../assets/shirts/3.png'
+
 export default function Home() {
-  return <h1>Hello world</h1>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} alt='' width={520} height={480} />
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} alt='' width={520} height={480} />
+        <footer>
+          <strong>Camiseta 2</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,68 @@
+import { styled } from '..'
+
+export const HomeContainer = styled('main', {
+  marginLeft: 'auto',
+  maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
+  minHeight: 656,
+  width: '100%',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'row',
+  gap: '1rem',
+})
+
+export const Product = styled('a', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  background: 'linear-gradient(180deg, #1EA483 0%, #7465D4 100%)',
+  borderRadius: 6,
+  cursor: 'pointer',
+  overflow: 'hidden',
+  padding: '0.25rem',
+  position: 'relative',
+
+  img: {
+    marginBottom: '1rem',
+    objectFit: 'fill',
+  },
+
+  footer: {
+    background: 'rgba(32, 32, 36, 0.9)',
+    borderRadius: 6,
+
+    position: 'absolute',
+    bottom: '0.25rem',
+    right: '0.25rem',
+    left: '0.25rem',
+    padding: '1rem',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    transform: 'translateY(110%)',
+    opacity: 0,
+    transition: 'all 0.2s ease-in-out',
+
+    strong: {
+      fontSize: '$lg',
+    },
+
+    span: {
+      color: '$green300',
+      fontSize: '$xl',
+      fontWeight: 'bold',
+    },
+  },
+
+  '&:hover': {
+    footer: {
+      transform: 'translateY(0%)',
+      opacity: 1,
+    },
+  },
+})
