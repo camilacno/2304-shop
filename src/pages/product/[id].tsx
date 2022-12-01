d49e0a639bfd357b11525e9888525d4a35de294b
Camila Nepomuceno <camila.cno@gmail.com>
2022-12-01 19:24:22 -0300

    Product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,25 @@
-import { useRouter } from 'next/router'
-import React from 'react'
+import {
+  ProductContainer,
+  ImageContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
 
 export default function Product() {
-  const { query } = useRouter()
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+      <ProductDetails>
+        <h1>Camiseta</h1>
+        <span>R$ 79,90</span>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          maxime debitis nihil labore soluta laboriosam adipisci fugiat
+          inventore voluptatum dolorem temporibus autem dignissimos,
+          perspiciatis totam, blanditiis laborum nesciunt impedit animi.
+        </p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
---
+++ src/styles/pages/product.ts
@@ -0,0 +1,70 @@
+import { styled } from '..'
+
+export const ProductContainer = styled('main', {
+  margin: '0 auto',
+  maxWidth: 1180,
+
+  display: 'grid',
+  gridTemplateColumns: 'repeat(2, 1fr)',
+  alignItems: 'stretch',
+  gap: '4rem',
+})
+
+export const ImageContainer = styled('div', {
+  background: 'linear-gradient(180deg, #1EA483 0%, #7465D4 100%)',
+  borderRadius: 6,
+  height: 656,
+  maxWidth: 576,
+  padding: '0.25rem',
+  width: '100%',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  img: {
+    objectFit: 'cover',
+  },
+})
+
+export const ProductDetails = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+
+  h1: {
+    fontSize: '$2xl',
+    color: '$gray300',
+  },
+
+  span: {
+    color: '$green300',
+    display: 'block',
+    fontSize: '$2xl',
+    marginTop: '1rem',
+  },
+
+  p: {
+    color: '$gray300',
+    fontSize: '$md',
+    lineHeight: 1.6,
+    marginTop: '2.5rem',
+  },
+
+  button: {
+    backgroundColor: '$green500',
+    border: 0,
+    borderRadius: 6,
+    color: '$white',
+    cursor: 'pointer',
+    fontSize: '$md',
+    fontWeight: 'bold',
+    marginTop: 'auto',
+    padding: '1.25rem',
+
+    transition: 'backgroundColor, 0.2s',
+
+    '&:hover': {
+      backgroundColor: '$green300',
+    },
+  },
+})
