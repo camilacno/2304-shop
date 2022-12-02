75304397c04f4db436f4367a0b79ffda1bb75ddc
Camila Nepomuceno <camila.cno@gmail.com>
2022-12-02 12:20:28 -0300

    SSR redirect

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -33,13 +33,21 @@ export default function Success({ customerName, product }: SuccessProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
   const sessionId = String(query.session_id)
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
     expand: ['line_items', 'line_items.data.price.product'],
   })
 
-  console.log(session.line_items.data[0].price.product)
   const customerName = session.customer_details.name
   const product = session.line_items.data[0].price.product as Stripe.Product
 
