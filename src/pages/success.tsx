61f7dcaf677c1ebfdb9d69f3572fc55fab71199b
Camila Nepomuceno <camila.cno@gmail.com>
2022-12-02 12:15:34 -0300

    Retrieve shopping info at checkout (success)

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,23 +1,55 @@
-import Image from 'next/image'
 import React from 'react'
+import { GetServerSideProps } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import Stripe from 'stripe'
+
+import { stripe } from '../libs/stripe'
 import { SuccessContainer, ImageContainer } from '../styles/pages/success'
 
-import logoImg from '../assets/logo.svg'
-import Link from 'next/link'
+interface SuccessProps {
+  customerName: string
+  product: {
+    name: string
+    imageUrl: string
+  }
+}
 
-export default function Success() {
+export default function Success({ customerName, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada!</h1>
       <ImageContainer>
-        <Image src={logoImg} alt='' width={114} height={106} />
+        <Image src={product.imageUrl} alt='' width={114} height={106} />
       </ImageContainer>
       <p>
-        Uhuul <strong>Camila Nepomuceno</strong>, sua{' '}
-        <strong>Camiseta Beyond</strong> já está a caminho da sua casa.
+        Uhuul <strong>{customerName}</strong>, sua{' '}
+        <strong>{product.name}</strong> já está a caminho da sua casa.
       </p>
 
       <Link href='/'>Voltar ao catálogo</Link>
     </SuccessContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id)
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  })
+
+  console.log(session.line_items.data[0].price.product)
+  const customerName = session.customer_details.name
+  const product = session.line_items.data[0].price.product as Stripe.Product
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  }
 }
